be9e4e3ad8b3689db9e0bc790c4452a9ea95e149
Victor <mxd46934@gmail.com>
2025-03-07 18:44:46 -0300

    Feature/0017: Configurations for swal easy-to-use;

--- swal.js
+++ swal.js
@@ -1,17 +1,38 @@
-function swal(verification, title, text) {
+// This is the SweetAlert easy-to-use function, include 
+// it in your footer page using script src tag!
+// 
+// @author Victor Béser
 
-    if(verification == true) {
-        Swal.fire({
-            title: title,
-            text: text,
-            icon: "success"
-          });
-    } else if(verification == false) {
-        Swal.fire({
-            title: title,
-            text: text,
-            icon: "error"
-          });
+function swal(verification, text, title) {
+
+    if (verification == true) {
+        if (title != null) {
+            Swal.fire({
+                title: title,
+                text: text,
+                icon: "success"
+            });
+        } else {
+            Swal.fire({
+                title: "Boa!",
+                text: text,
+                icon: "success"
+            });
+        }
+    } else if (verification == false) {
+        if (title != null) {
+            Swal.fire({
+                title: title,
+                text: text,
+                icon: "error"
+            });
+        } else {
+            Swal.fire({
+                title: "Oops!",
+                text: text,
+                icon: "error"
+            });
+        }
     } else {
         console.log("Not a valid value fo verification variable!");
         return;
